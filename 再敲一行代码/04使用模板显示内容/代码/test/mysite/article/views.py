9966b2a450ab69a4b6590b156d45fb6dd29ff800
LuoSue <hur73426@gmail.com>
2021-09-06 21:51:46 +0800

    第四章

---
+++ 再敲一行代码/04使用模板显示内容/代码/test/mysite/article/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Article
+# Create your views here.
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    context = {}
+    context['article_obj'] = article
+    return render(request,"article_detail.html", context)
+def article_list(request):
+    articles = Article.objects.all()
+    context = {}
+    context['articles'] = articles
+    return render(request,"article_list.html",context)
